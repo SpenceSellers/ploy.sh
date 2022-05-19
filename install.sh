c4364fc17bf93ab0df5260460819c6ed377b5875
Spencer Sellers <spencesellers@gmail.com>
2022-05-18 21:30:10 -0500

    Add message after install

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Nuke the current git data that we got from cloning this project.
+# It's not possible to be both a bare and non-bare git repo at the same time.
+# (I tried.)
 rm -rf .git
 
 git init --bare
 mv post-receive hooks/post-receive
+
+PWD=$(pwd)
+echo "Ploy installed. To add a remote, run:"
+echo "git remote add deploy ssh:$USER@SERVER_URL$PWD"
